6f843b79279fa791bc8c826d0e523176d249cdd4
Neutreing <preciousoloye@gmail.com>
2023-10-06 16:40:40 -0700

    2 task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase
+ *
+ * Return: always o
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
