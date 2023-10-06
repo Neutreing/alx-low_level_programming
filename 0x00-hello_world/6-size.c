b009c37e92d67d02bc5e7d8cb371665a13631c0f
Neutreing <preciousoloye@gmail.com>
2023-10-06 03:42:42 -0700

    6 task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 /**
- * main - this sdesribes the main function of the code
+ * main - Entry point
+ *
  * Return: 0
- 	*/
+ */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
-
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
