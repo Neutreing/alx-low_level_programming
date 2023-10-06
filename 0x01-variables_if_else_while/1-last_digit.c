fab9e30d00bcda13794cdf4c3f64d8bfe9bf87e5
Neutreing <preciousoloye@gmail.com>
2023-10-06 16:23:31 -0700

    1 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,14 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - starting point
+ * main - entry point
  * Return: always 0
  */
+
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
