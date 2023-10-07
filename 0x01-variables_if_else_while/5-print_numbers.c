1df5b1096d9f4742141eaac018ddb7ab5f0ef75e
Neutreing <preciousoloye@gmail.com>
2023-10-07 03:19:54 -0700

    5 task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- * main - program that prints the alphabet in lowercase and upper case
- *
- * Return: Always 0.
- */
+* main - program that prints the alphabet in lower and upper case
+* Return: always 0.
+*/
+
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+
 	{
 	if (letter != 'e' && letter != 'q')
 	putchar(letter);
 	}
+
 	putchar('\n');
 
 	return (0);
 }
-
