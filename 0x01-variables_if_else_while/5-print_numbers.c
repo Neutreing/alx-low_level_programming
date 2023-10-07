9869b04d0abe82bb1e8cd02977ec92a89789de05
Neutreing <preciousoloye@gmail.com>
2023-10-07 04:54:14 -0700

    5 task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,12 @@
 * main - program that prints sinngle digit number of base 10 starting from 0
 * Return: always 0.
 */
-int main() 
+int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d\n", i);
 	}
-	return 0;
+	return (0);
 }
