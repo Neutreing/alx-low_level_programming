35b521bc5c84bd702a4e573f210b5f30a2851e3e
Neutreing <preciousoloye@gmail.com>
2023-10-10 23:35:09 -0700

    1 task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,10 @@
 /**
  * main - print all aplhabet
  */
-void print_alphabet(v0id)
+void print_alphabet(void)
 {
 	char letter;
+
 	for (letter = 'a'' letter <='z'' letter++)
 		_putchar(letter);
 	_putchar('\n');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define main_h
 
 int _putchar(char);
-
+int void print_alphabet(void);
 #endif
